f271a26b06219895f226f4f6415c3fc60e14eee9
adammharris <harrisadam42103@gmail.com>
2025-04-09 12:54:11 -0600

    Automatically collapse navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,23 +7,60 @@ import { useAuth } from '../contexts/UserContext';
 
 const Navigation: React.FC = () => {
   const { loggedIn } = useAuth();
-  
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar expand="sm" className="bg-body-secondary" fixed="top">
+    <Navbar
+      expand="sm"
+      className="bg-body-secondary"
+      fixed="top"
+      expanded={expanded}
+      collapseOnSelect
+      onSelect={() => setExpanded(false)}
+    >
       <Container>
-        <Navbar.Brand>ShowBrain</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand
+          as={NavLink}
+          to="/"
+          onClick={() => setExpanded(false)}
+        >
+          ShowBrain
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/" onClick={() => setExpanded(false)}>
+              Home
+            </Nav.Link>
             {loggedIn ? (
-              <Nav.Link as={NavLink} to="/dashboard">Dashboard</Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                to="/dashboard"
+                onClick={() => setExpanded(false)}
+              >
+                Dashboard
+              </Nav.Link>
             ) : (
-              <Nav.Link as={NavLink} to="login">Login</Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                to="/login"
+                onClick={() => setExpanded(false)}
+              >
+                Login
+              </Nav.Link>
             )}
-            <Nav.Link as={NavLink} to="blog">Blog</Nav.Link>
-            <Nav.Link as={NavLink} to="about">About</Nav.Link>
-            <Nav.Link as={NavLink} to="help">Help</Nav.Link>
+            <Nav.Link as={NavLink} to="/blog" onClick={() => setExpanded(false)}>
+              Blog
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about" onClick={() => setExpanded(false)}>
+              About
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/help" onClick={() => setExpanded(false)}>
+              Help
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
